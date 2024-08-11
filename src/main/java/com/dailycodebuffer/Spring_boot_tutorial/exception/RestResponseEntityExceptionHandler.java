709335f58061560bf0650c559788fe34bbe513f7
Syed Shubair <shubair@chargeup.club>
2024-08-11 19:19:38 +0500

    Added Actuator with custom routes and properties.

--- src/main/java/com/dailycodebuffer/Spring_boot_tutorial/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/dailycodebuffer/Spring_boot_tutorial/exception/RestResponseEntityExceptionHandler.java
@@ -2,26 +2,19 @@ package com.dailycodebuffer.Spring_boot_tutorial.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 @ResponseStatus
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-
     @ExceptionHandler(DepartmentNotFoundException.class)
-    public ResponseEntity<ErrorMessage> departmentNotFoundException(DepartmentNotFoundException departmentNotFoundException, WebRequest req) {
+    public ResponseEntity<ErrorMessage> departmentNotFoundException(DepartmentNotFoundException departmentNotFoundException) {
         ErrorMessage errorMessage = ErrorMessage.builder().message(departmentNotFoundException.getMessage()).status(HttpStatus.NOT_FOUND).build();
-        return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
     }
 
 }
